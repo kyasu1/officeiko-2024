1e84c79e2376e2ed0c8cc24efb3a12eaebf5d608
Yasuyuki Komatsubara <kyasuyako@gmail.com>
2024-03-15 12:46:57 +0900

    add types

--- src/Layout/Access.elm
+++ src/Layout/Access.elm
@@ -7,6 +7,8 @@ import Html.Attributes as A exposing (class)
 import Json.Decode as JD
 import Layout
 import Markdown
+import Markdown.Block
+import Markdown.Html
 import Markdown.Renderer
 import Strapi
 import Url
@@ -78,6 +80,35 @@ row field =
         ]
 
 
+descriptionRender :
+    { heading :
+        { level : Markdown.Block.HeadingLevel
+        , rawText : String
+        , children : List (Html msg)
+        }
+        -> Html msg
+    , blockQuote : List (Html msg) -> Html msg
+    , html : Markdown.Html.Renderer (List (Html msg) -> Html msg)
+    , text : String -> Html msg
+    , codeSpan : String -> Html msg
+    , strong : List (Html msg) -> Html msg
+    , emphasis : List (Html msg) -> Html msg
+    , strikethrough : List (Html msg) -> Html msg
+    , hardLineBreak : Html msg
+    , image : { alt : String, src : String, title : Maybe String } -> Html msg
+    , unorderedList : List (Markdown.Block.ListItem (Html msg)) -> Html msg
+    , orderedList : Int -> List (List (Html msg)) -> Html msg
+    , codeBlock : { body : String, language : Maybe String } -> Html msg
+    , thematicBreak : Html msg
+    , table : List (Html msg) -> Html msg
+    , tableHeader : List (Html msg) -> Html msg
+    , tableBody : List (Html msg) -> Html msg
+    , tableRow : List (Html msg) -> Html msg
+    , tableCell : Maybe Markdown.Block.Alignment -> List (Html msg) -> Html msg
+    , tableHeaderCell : Maybe Markdown.Block.Alignment -> List (Html msg) -> Html msg
+    , paragraph : List (Html msg) -> Html msg
+    , link : { title : Maybe String, destination : String } -> List (Html msg) -> Html msg
+    }
 descriptionRender =
     Markdown.Renderer.defaultHtmlRenderer
         |> (\r ->
@@ -103,6 +134,35 @@ descriptionRender =
            )
 
 
+stepRender :
+    { heading :
+        { level : Markdown.Block.HeadingLevel
+        , rawText : String
+        , children : List (Html msg)
+        }
+        -> Html msg
+    , blockQuote : List (Html msg) -> Html msg
+    , html : Markdown.Html.Renderer (List (Html msg) -> Html msg)
+    , text : String -> Html msg
+    , codeSpan : String -> Html msg
+    , strong : List (Html msg) -> Html msg
+    , emphasis : List (Html msg) -> Html msg
+    , strikethrough : List (Html msg) -> Html msg
+    , hardLineBreak : Html msg
+    , image : { alt : String, src : String, title : Maybe String } -> Html msg
+    , unorderedList : List (Markdown.Block.ListItem (Html msg)) -> Html msg
+    , orderedList : Int -> List (List (Html msg)) -> Html msg
+    , codeBlock : { body : String, language : Maybe String } -> Html msg
+    , thematicBreak : Html msg
+    , table : List (Html msg) -> Html msg
+    , tableHeader : List (Html msg) -> Html msg
+    , tableBody : List (Html msg) -> Html msg
+    , tableRow : List (Html msg) -> Html msg
+    , tableCell : Maybe Markdown.Block.Alignment -> List (Html msg) -> Html msg
+    , tableHeaderCell : Maybe Markdown.Block.Alignment -> List (Html msg) -> Html msg
+    , paragraph : List (Html msg) -> Html msg
+    , link : { title : Maybe String, destination : String } -> List (Html msg) -> Html msg
+    }
 stepRender =
     Markdown.Renderer.defaultHtmlRenderer
         |> (\r ->
